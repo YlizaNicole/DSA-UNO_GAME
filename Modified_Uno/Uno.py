c1ba05481d61da07a7bde8a92efc3dda9643886e
Yli <salazarylizanicole@gmail.com>
2023-02-15 09:56:44 +0800

    adding the project

---
+++ Modified_Uno/Uno.py
@@ -0,0 +1,9 @@
+####### TRYING TO IMPROVE THE SRC CODE ########
+#making it a single player
+
+
+#STEPS
+#Make it open a program that houses the game
+#build a deck
+#add user interface
+#functions 
